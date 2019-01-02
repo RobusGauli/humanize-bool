427df936d2e466f755fb3fc9b834b6571d5bd81b
Robus Gauli <robusgauli@gmail.com>
2019-01-02 22:18:34 +0545

    Initial commit

--- src/index.js
+++ src/index.js
@@ -7,6 +7,81 @@
  * LICENSE.txt file in the root directory of this source tree.
  */
 
-import Greeting from './Greeting';
+function is() {
+  const args = Array.prototype.slice.call(arguments);
+  // flags constants
+  const NOT = 'NOT';
+  const ANY = 'ANY';
+  const ALL = 'ALL';
 
-export default Greeting;
+  const OF = 'OF';
+
+  function setCurrentFlag(flag) {
+    return function wrapper(target, proxyInstance) {
+      target.currentFlag = flag;
+      return proxyInstance;
+    };
+  }
+
+  const flags = {
+    not: setCurrentFlag(NOT),
+    any: setCurrentFlag(ANY),
+    all: setCurrentFlag(ALL),
+    of: setCurrentFlag(OF),
+  };
+
+  // proxy object to return
+  const proxyInstance = new Proxy({
+    currentFlag: ALL,
+    instanceof(protoType) {
+      // compute the flags
+      const computedFlags = args
+        .map(arg => arg instanceof protoType);
+
+      return this[this.currentFlag](computedFlags);
+    },
+    equalTo(value) {
+      const computedFlags = args
+        .map(arg => arg === value);
+
+      return this[this.currentFlag](computedFlags);
+    },
+    typeof(type) {
+      const computedFlags = args.map(arg => typeof arg === type);
+      return this[this.currentFlag](computedFlags);
+    },
+    length(len) {
+      if (typeof len !== 'number') {
+        throw new TypeError('Len argument must be of type Number.');
+      }
+      if (!args.length) {
+        return args.length === len;
+      }
+      if (Array.isArray(args[0])) {
+        return args[0].length === len;
+      }
+      return args.length === len;
+    },
+    ALL(computedFlags) {
+      return computedFlags.reduce((acc, val) => acc && val, true);
+    },
+    NOT(computedFlags) {
+      return !this.ALL(computedFlags);
+    },
+    ANY(computedFlags) {
+      return computedFlags.reduce((acc, val) => acc || val, false);
+    },
+  }, {
+    // traps
+    get(target, name) {
+      if (flags[name]) {
+        return flags[name](target, proxyInstance);
+      }
+      return target[name];
+    },
+  });
+
+  return proxyInstance;
+}
+
+export default is;
