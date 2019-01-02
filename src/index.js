7f5c47a0484c33ca26f99dd02b188fc2393212da
Robus Gauli <robusgauli@gmail.com>
2019-01-02 22:22:15 +0545

    Add license and author

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 /**
- * Babel Starter Kit (https://www.kriasoft.com/babel-starter-kit)
+ * Is (Robus Gauli robusgauli@gmail.com)
  *
- * Copyright © 2015-2016 Kriasoft, LLC. All rights reserved.
+ * Copyright © 2018-2019 Robus Gauli. All rights reserved.
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE.txt file in the root directory of this source tree.
