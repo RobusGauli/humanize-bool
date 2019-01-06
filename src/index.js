4882dc31e2087f10a58fd909ee7c4cd7a811ae86
Robus Gauli <robusgauli@gmail.com>
2019-01-06 22:03:01 +0545

    user defined and library builtin functions

--- src/index.js
+++ src/index.js
@@ -10,30 +10,95 @@
 
 
 function is() {
-  // lets do something really quicky
+  const hasProp = Object.prototype.hasOwnProperty;
+  const args = Array.prototype.slice.call(arguments)
 
-    const obj = {
-      isValueCheck: true,
-      unionFlags: [],
-      undefined: function() {
-        this.unionFlags.push('undefined');
-      },
-      null: function() {
-        this.unionFlags.push('null')
-      },
-      type: function() {
-          this.isValueCheck = false;
-      },
-    }
+  // types and value for a given JS data
+  const valuesMap = {
+    undefined: undefined,
+    null: null,
+  }
+  const typesMap = {
+    undefined: 'undefined',
+    object: 'object',
+    boolean: 'boolean',
+    symbol: 'symbol',
+    function: 'function'
+  }
 
-    function compute() {
-      
+  const obj = {
+    all: true,
+    and: true,
+    values: [],
+    types: [],
+    isValueCheck: true,
+    undefined: function () {
+      if (this.isValueCheck) {
+        this.values.push(valuesMap.undefined);
+      } else {
+        this.types.push(typesMap.undefined);
+      }
+    },
+    null: function () {
+      if (this.isValueCheck) {
+        this.values.push(valuesMap.null);
+      }
+    },
+    object: function() {
+      if(!this.isValueCheck) {
+        this.types.push(typesMap.object);
+      }
+    },
+    boolean: function() {
+      if (!this.isValueCheck) {
+        this.types.push(typesMap.boolean)
+      }
+    },
+    symbol: function() {
+      if (!this.isValueCheck) {
+        this.types.push(typesMap.symbol);
+      }
+    },
+    func: function() {
+      if (!this.isValueCheck) {
+        this.types.push(typesMap.function)
+      }
+    },
+    type: function () {
+      this.isValueCheck = false;
+    },
+    or: function() {
+      this.and = false;
+    },
+    any: function() {
+      this.all = false;
     }
+  }
+
+  function compute(values) {
+    // need to evaluate the expression  
+    return obj.isValueCheck
+      ? valueCompute(values)
+      : typeCompute(values)
+  }
+
+  function typeCompute(...values) {
+    const computedFlags = obj.unionFlags.map(flag => args.map(arg => typeof arg === flag));
+    const userComputedFlags = values.map(value => args.map(arg => typeof arg === value));
+    
+    console.log(userComputedFlags);
+    console.log(computedFlags);
+    return 'type';
+  }
+
+  function valueCompute(value) {
+    return 'value'
+  }
   // what i wanna do it return the function as an proxy back to the user
-  const proxy =  new Proxy(compute, {
+  const proxy = new Proxy(compute, {
     get(target, name) {
       if (obj[name]) {
-        obj[name]();
+        obj[name].call(obj)
       }
       return proxy;
     }
