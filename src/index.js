176b21f888d6f89a247df161095816d3e1c0f593
Robus Gauli <robusgauli@gmail.com>
2019-01-08 11:16:35 +0545

    Added examples

--- src/index.js
+++ src/index.js
@@ -7,6 +7,37 @@
  * LICENSE.txt file in the root directory of this source tree.
  */
 
+/**
+ *
+ * @param {any} arg
+ * @returns {object} proxy
+ *
+ * USAGE:
+ * 1. check weather the value is undefined or null?
+ *
+ * const result = is(value).undefined.or.null()
+ *
+ * 2. Check weather the value is number or string?
+ *
+ * const result = is(value).number.or.string()
+ *
+ * 3. Check weather the value is of type function?
+ *
+ * const result = is(value).type.func();
+ *
+ * 4. Check weather is value is of type string or number?
+ *
+ * const result = is(val).of.type.string.or.number()
+ * OR
+ * const result = is(val).any.type.string.number()
+ * OR
+ * const result = is(val).any.type('string', 'number')
+ *
+ * 5. Check weather value is either cat, dog, car, or robus
+ *
+ * const result = is(val).any.equalsTo('cat', 'dog', 'car', 'robus')
+ *
+ */
 export default function is(arg) {
   // Initial Evaluation state
   const evaluationState = {
@@ -14,80 +45,78 @@ export default function is(arg) {
     values: [],
     types: [],
     isValueCheck: true,
-    undefined: function () {
+    undefined: function() {
       if (this.isValueCheck) {
-
         this.values.push(undefined);
       } else {
         this.types.push('undefined');
       }
     },
-    null: function () {
+    null: function() {
       if (this.isValueCheck) {
         this.values.push(null);
       }
     },
-    string: function () {
+    string: function() {
       if (!this.isValueCheck) {
         this.types.push('string');
       }
     },
-    number: function () {
+    number: function() {
       if (!this.isValueCheck) {
-        this.types.push('number')
+        this.types.push('number');
       }
     },
-    object: function () {
+    object: function() {
       if (!this.isValueCheck) {
         this.types.push('object');
       }
     },
-    boolean: function () {
+    boolean: function() {
       if (!this.isValueCheck) {
-        this.types.push('boolean')
+        this.types.push('boolean');
       }
     },
-    symbol: function () {
+    symbol: function() {
       if (!this.isValueCheck) {
         this.types.push('symbol');
       }
     },
-    func: function () {
+    func: function() {
       if (!this.isValueCheck) {
-        this.types.push('function')
+        this.types.push('function');
       }
     },
-    type: function () {
+    type: function() {
       this.isValueCheck = false;
     },
-    or: function () {
+    or: function() {
       this.all = false;
     },
-    any: function () {
+    any: function() {
       this.all = false;
     }
-  }
+  };
 
   /**
    * Evaluator decorator takes predicate as an input and returns the function that evaluates to an expression given a predicate.
-   *  
+   *
    * @param {function} predicate
-   * @returns {function} 
+   * @returns {function}
    */
 
-  const evaluator = predicate => (...values) => 
-    evaluationState.all 
-      ? values.map(predicate).reduce((acc, flag) => acc && flag, true) 
+  const evaluator = predicate => (...values) =>
+    evaluationState.all
+      ? values.map(predicate).reduce((acc, flag) => acc && flag, true)
       : values.map(predicate).reduce((acc, flag) => acc || flag, false);
 
-  
   // This function evaluates to boolean expression given a values.
   const evaluateValue = evaluator(value => arg === value);
-  // This function evaluates to boolean expression given a types. 
+  // This function evaluates to boolean expression given a types.
   const evaluateType = evaluator(value => typeof arg === value);
 
   // Function that is called by the user code
-  const evaluate = (...values) => 
+  const evaluate = (...values) =>
     evaluationState.isValueCheck
       ? evaluateValue(...values, ...evaluationState.values)
       : evaluateType(...values, ...evaluationState.types);
@@ -96,13 +125,12 @@ export default function is(arg) {
   const proxy = new Proxy(evaluate, {
     get(target, name) {
       if (evaluationState[name]) {
-        evaluationState[name].call(evaluationState)
+        evaluationState[name].call(evaluationState);
       }
+
       return proxy;
     }
-  })
+  });
 
   return proxy;
 }
-
-
